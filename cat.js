12ffd446fe1f15892187a4f30da06012ac7b638f
Aditya Soni <77341618+Aditya-Soni23@users.noreply.github.com>
2023-10-12 22:03:28 +0530

    Add files via upload

---
+++ cat.js
@@ -0,0 +1,18 @@
+function buy(){
+    window.location = "payment.html";
+}
+function getcat(){
+    window.location = "catalogs.html";
+}
+function home(){
+    window.location = "index.html";
+}
+function verify(){
+    window.location = "indexsend.html";
+}
+function custom(){
+    window.location = "cus.html";
+}
+function con(){
+    window.location = "cont.html";
+}
